613bc7186887434f65c27602528a9f6ee7551ad3
Gladiné <kjell.gladine1@gmail.com>
2024-11-20 18:02:17 +0100

    refactor: Improve readability

---
+++ Santa.h
@@ -0,0 +1,47 @@
+//
+// Created by RMDY on 14/11/2024.
+//
+
+#ifndef AOC_H
+#define AOC_H
+
+class Santa {
+public:
+    int calculateFloorBasedOnInput(string input);
+
+    int calculateFirstEncounterWithBasement(string input);
+};
+
+int Santa::calculateFloorBasedOnInput(string input) {
+    int count_of_opening = 0;
+    int count_of_closing = 0;
+    for (int counter = 0; counter < input.length(); counter++) {
+        char i = input[counter];
+        if (i == '(') {
+            count_of_opening++;
+        } else if (i == ')') {
+            count_of_closing++;
+        }
+    }
+    return count_of_opening - count_of_closing;
+}
+
+int Santa::calculateFirstEncounterWithBasement(string input) {
+    int count_of_opening = 0;
+    int count_of_closing = 0;
+    for (int counter = 0; counter < input.length(); counter++) {
+        char i = input[counter];
+        if (i == '(') {
+            count_of_opening++;
+        } else if (i == ')') {
+            count_of_closing++;
+        }
+        if (count_of_closing > count_of_opening && count_of_opening - count_of_closing == -1) {
+            return counter + 1;
+        }
+    }
+    return -1;
+}
+
+
+#endif //AOC_H
