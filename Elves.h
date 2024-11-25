c4b67950d182e177de108e5ec2290b24ab50350b
Kjell Gladine <kjell.gladine@just.fgov.be>
2024-11-25 06:18:37 +0100

    Revert "task: calculate correct values"
    This reverts commit 54b7af93bd2474dde166e9aa9997114b6107f03b.

--- Elves.h
+++ Elves.h
@@ -7,45 +7,22 @@
 
 
 class Elves {
-    static int getCubicFeetInVolume(int length, int height, int width);
-    static int calculateRibbonToWrap(int length, int height, int width);
-public:
-    static int calculateBox(int length ,int height, int width);
-    static int calculateRibbon(int length ,int height, int width);
-
+   int calculateBox(int length ,int height, int width);
 };
 
-inline int Elves::getCubicFeetInVolume(int length, int height, int width) {
-    return (length * width * height);
-}
-
-inline int Elves::calculateRibbon(int length ,int height, int width) {
-    return calculateRibbonToWrap(length,width,height) + getCubicFeetInVolume(length, height, width);
-}
-
-inline int Elves::calculateBox(int length, int height, int width) {
+int Elves::calculateBox(int length, int height, int width) {
     int slack = 0;
     int SIDE_ONE = length * width;
     int SIDE_TW0 = width * height;
     int SIDE_THREE = height * length;
-    if (SIDE_THREE < SIDE_ONE && SIDE_THREE < SIDE_TW0) {
+    if (SIDE_THREE > SIDE_ONE && SIDE_THREE > SIDE_TW0) {
         slack = SIDE_THREE;
-    } else if (SIDE_TW0 < SIDE_ONE) {
+    } else if (SIDE_TW0 > SIDE_ONE) {
         slack = SIDE_TW0;
     } else {
         slack = SIDE_ONE;
     }
     return (2 * SIDE_ONE) + (2 *SIDE_TW0) + (2 * SIDE_THREE) + slack;
-}
-
-inline int Elves::calculateRibbonToWrap(int length, int height, int width) {
-    if(length >= width && length >= height) {
-        return (width + width + height + height);
-    } else if(width >= height) {
-        return (length + length + height + height);
-    } else {
-        return (width + width + length + length);
-    }
 }
 
 
